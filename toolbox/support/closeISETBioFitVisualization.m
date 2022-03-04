80b31b32c43ad218c687b8153b7b37318b5931e9
Nicolas Cottaris <cottaris@upenn.edu>
2022-03-04 15:01:01 -0500

    Deleting previous state

--- toolbox/support/closeISETBioFitVisualization.m
+++
@@ -1,7 +0,0 @@
-function closeISETBioFitVisualization(visStruct)
-    % Close video
-    visStruct.videoOBJ.close();
-
-    % Export PDF
-    NicePlot.exportFigToPDF(visStruct.pdfFilename, visStruct.hFig, 300);
-end
--- toolbox/support/coneMosaicFilename.m
+++
@@ -1,4 +0,0 @@
-function filename = coneMosaicFilename(monkeyID)
-    rootDirName = ISETmacaqueRootPath();
-    filename = fullfile(rootDirName, sprintf('dataResources/coneMosaic%s.mat', monkeyID));
-end
