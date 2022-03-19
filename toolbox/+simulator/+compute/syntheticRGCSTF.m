f4c0df0540aaec14da76893b778c383eae3ef0ec
Nicolas Cottaris <cottaris@upenn.edu>
2022-03-19 13:03:32 -0400

    updates

--- toolbox/+simulator/+compute/syntheticRGCSTF.m
+++ toolbox/+simulator/+compute/syntheticRGCSTF.m
@@ -36,12 +36,17 @@ function syntheticRGCSTF(syntheticRGCmodelFilename, coneMosaicResponsesFileName,
 
     % Load the RGC model to employ
     load(syntheticRGCmodelFilename, 'fittedModels');
+    rfCenterConePoolingScenario
+    pause
     coneMosaicPositionModels = fittedModels(rfCenterConePoolingScenario);
 
     % This model contains fits to a number of cone positions.
     % Exctract the model at the position which results in the min RMS
     bestConePosIdx = simulator.analyze.bestConePositionAcrossMosaic(...
             coneMosaicPositionModels, STFdataToFit, rmsSelector);
+
+    hFig = figure(1); clf;
+    for bestConePosIdx = 1:numel(coneMosaicPositionModels)
     theBestConePositionModel = coneMosaicPositionModels{bestConePosIdx};
 
     theBestConePositionModel
@@ -53,49 +58,85 @@ function syntheticRGCSTF(syntheticRGCmodelFilename, coneMosaicResponsesFileName,
     surroundConeIndices = theBestConePositionModel.fittedRGCRF.surroundConeIndices;
     surroundConeWeights = theBestConePositionModel.fittedRGCRF.surroundConeWeights;
 
+    integratedSensSurroundToCenter = theBestConePositionModel.DoGparams.bestFitValues(2) * (theBestConePositionModel.DoGparams.bestFitValues(3))^2
+    weightsSurroundToCenter = sum(surroundConeWeights)/sum(centerConeWeights)
+    
+    
     centerConeIndices = centerConeIndices(:);
     centerConeWeights = centerConeWeights(:);
     surroundConeIndices = surroundConeIndices(:);
     surroundConeWeights = surroundConeWeights(:);
 
-    %figure();
+  %  figure();
     for iSF = 1:numel(spatialFrequenciesExamined)
         for tBin = 1:numel(temporalSupportSeconds)
             cMosaicSpatialActivation = squeeze(coneMosaicSpatiotemporalActivation(iSF,tBin,:));
             centerConeSignals = cMosaicSpatialActivation(centerConeIndices);
             surroundConeSignals = cMosaicSpatialActivation(surroundConeIndices);
-            centerActivation = sum(centerConeSignals(:) .* centerConeWeights(:));
-            surroundActivation = sum(surroundConeSignals(:) .* surroundConeWeights(:));
-            RGCresponse(iSF,tBin) = centerActivation - surroundActivation;
+            centerResponse(tBin) = sum(centerConeSignals(:) .* centerConeWeights(:));
+            surroundResponse(tBin) = sum(surroundConeSignals(:) .* surroundConeWeights(:));
         end 
-%         subplot(3,5, iSF)
-%         plot(temporalSupportSeconds, RGCresponse(iSF,:), 'k-');
-%         title(sprintf('%2.3f c/deg', spatialFrequenciesExamined(iSF)));
+        RGCresponse = centerResponse - surroundResponse;
+        
+        [~, fittedParams] = ...
+            simulator.fit.sinusoidToResponseTimeSeries(...
+                temporalSupportSeconds, ...
+                RGCresponse, ...
+                WilliamsLabData.constants.temporalStimulationFrequencyHz, ...
+                []);
+        theSynthesizedSTF(iSF) = fittedParams(1);
+
+        [~, fittedParams] = ...
+            simulator.fit.sinusoidToResponseTimeSeries(...
+                temporalSupportSeconds, ...
+                centerResponse, ...
+                WilliamsLabData.constants.temporalStimulationFrequencyHz, ...
+                []);
+        
+        theCenterSTF(iSF) = fittedParams(1);
+
+        [~, fittedParams] = ...
+            simulator.fit.sinusoidToResponseTimeSeries(...
+                temporalSupportSeconds, ...
+                surroundResponse, ...
+                WilliamsLabData.constants.temporalStimulationFrequencyHz, ...
+                []);
+        theSurroundSTF(iSF) = fittedParams(1);
+        
+%          subplot(3,5, iSF)
+%          plot(temporalSupportSeconds, RGCresponse, 'k-');
+%          set(gca, 'YLim', [-0.2 0.2])
+%          title(sprintf('%2.3f c/deg', spatialFrequenciesExamined(iSF)));
 
     end
 
-    theSynthesizedSTF = max(RGCresponse,[],2);
-    hFig = figure()
-    subplot(1,3,1)
+    
+    subplot(7,3,(bestConePosIdx-1)*3+1)
     plot(spatialFrequenciesExamined, theSynthesizedSTF, 'ks-');
+    hold on;
+    plot(spatialFrequenciesExamined, theCenterSTF, 'r.-');
+    plot(spatialFrequenciesExamined, theSurroundSTF, 'b.-');
     set(gca, 'XScale', 'log', 'XLim', [4 60])
-    subplot(1,3,2)
+
+    subplot(7,3,(bestConePosIdx-1)*3+2)
     plot(spatialFrequenciesExamined, STFdataToFit.responses, 'ro');
     hold on
     plot(spatialFrequenciesExamined, theBestConePositionModel.fittedSTF, 'r-');
     set(gca, 'XScale', 'log', 'XLim', [4 60])
 
-    theSynthesizedSTF = (theSynthesizedSTF-min(theSynthesizedSTF))/(max(theSynthesizedSTF)-min(theSynthesizedSTF));
-    m1 = min(STFdataToFit.responses);
-    m2 = max(STFdataToFit.responses);
-    STFdataToFit.responses = (STFdataToFit.responses-m1)/(m2-m1);
-    theBestConePositionModel.fittedSTF = (theBestConePositionModel.fittedSTF-m1)/(m2-m1);
-    subplot(1,3,3);
+    
+    subplot(7,3,(bestConePosIdx-1)*3+3);
+    yyaxis('left')
     plot(spatialFrequenciesExamined, theSynthesizedSTF, 'ks-');
-    hold on;
+    yyaxis('right')
     plot(spatialFrequenciesExamined, STFdataToFit.responses, 'ro');
+    hold on
     plot(spatialFrequenciesExamined, theBestConePositionModel.fittedSTF, 'r-');
     set(gca, 'XScale', 'log', 'XLim', [4 60], 'YLim', [0 1])
+
+    pause
+
+    end
 
     NicePlot.exportFigToPDF(syntheticSTFdataPDFFilename, hFig, 300);
 end
