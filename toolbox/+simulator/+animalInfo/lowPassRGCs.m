41cd385ce0675ac09a7df7f0854fce3192e28d53
Nicolas Cottaris <nicolas@Santorini.local>
2022-06-10 16:35:27 -0400

    Return center cone types and RGC indices
    Signed-off-by: Nicolas Cottaris <nicolas@Santorini.local>

--- toolbox/+simulator/+animalInfo/lowPassRGCs.m
+++ toolbox/+simulator/+animalInfo/lowPassRGCs.m
@@ -1,9 +1,10 @@
 % simulator.animalInfo.lowPassRGCs(monkeyID)
-function RGCIDs = lowPassRGCs(monkeyID)
+function [RGCIDs, centerConeTypes, coneRGCindices] = lowPassRGCs(monkeyID)
     switch (monkeyID)
         case 'M838'
             RGCIDs = {'L2', 'L9',  'M3'};
-
+            centerConeTypes = {'L', 'L', 'M'};
+            coneRGCindices = [2 9 3];
         otherwise
             error('No data for monkey ''%s''.', monkeyID);
     end
